bcd753c6197fb36cc74b65890faeac4e216013ee
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-10-30 10:07:48 -0600

    arrays and structs

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -4,11 +4,27 @@ pragma solidity 0.8.18; // stating our version
 contract SimpleStorage {
     // Basic types: boolean, uint, int, address, bytes
     // State variable - visibility docs: https://docs.soliditylang.org/en/latest/contracts.html#visibility-and-getters
-    uint256 public favoriteNumber; // 0
+    uint256 public myFavoriteNumber; // 0
+
+    // uint256 listOfFavoriteNumbers; 
+
+    // Creating types - favoriteNumber at index 0, string at index 2
+    struct Person{
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    // Creating an array of type People – Dynamic Array 
+    Person[] public listOfPeople; // []
+
+    // Can create a static array with a max size:
+    // Person[3] public listOfPeople;
+    
+    // Person public myFriend = Person({favoriteNumber: 11, name: "Jack"});
 
     // store a new favorite number
     function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber;
+        myFavoriteNumber = _favoriteNumber;
     }
 
     // functions that dont have to run for you to call them - view, pure
@@ -16,6 +32,10 @@ contract SimpleStorage {
     // view - read state on the blockchain; disallows state changes
     // pure - disallow reading state and stage changes 
     function retrieve() public view returns (uint256){
-        return favoriteNumber;
+        return myFavoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber, _name));
     }
 } 
