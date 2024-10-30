e11123519103ff8b0bca4636a80c72c952242806
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-10-30 09:46:45 -0600

    functions + view, pure

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -3,10 +3,19 @@ pragma solidity 0.8.18; // stating our version
 
 contract SimpleStorage {
     // Basic types: boolean, uint, int, address, bytes
-    uint256 favoriteNumber; // 0
+    // State variable - visibility docs: https://docs.soliditylang.org/en/latest/contracts.html#visibility-and-getters
+    uint256 public favoriteNumber; // 0
 
     // store a new favorite number
     function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber
+        favoriteNumber = _favoriteNumber;
+    }
+
+    // functions that dont have to run for you to call them - view, pure
+    // view and pure do cost gas ONLY when a gas-cost transaction is calling it
+    // view - read state on the blockchain; disallows state changes
+    // pure - disallow reading state and stage changes 
+    function retrieve() public view returns (uint256){
+        return favoriteNumber;
     }
 } 
