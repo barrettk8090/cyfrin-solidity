218a20bba4fabf30e14f6979f0b75a404164982d
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-11-07 09:14:19 -0700

    init storagefactory + update versions

---
+++ StorageFactory.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+contract StorageFactory{
+
+    function createSimpleStorageContract() public {
+        // How can this contract be made aware of the SimpleStorage contract? 
+    }
+}
