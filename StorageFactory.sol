fd537a451b6ebdb5ceb715d41c017eded2907f90
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-11-07 09:22:43 -0700

    deploying contracts from contracts

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -2,9 +2,42 @@
 
 pragma solidity ^0.8.18;
 
+contract SimpleStorage {
+
+    uint256 public myFavoriteNumber;
+
+    struct Person{
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    Person[] public listOfPeople;
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        myFavoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns (uint256){
+        return myFavoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
+
 contract StorageFactory{
 
+    // uint256 public favoriteNumber
+    // type visibility name 
+
+    SimpleStorage public simpleStorage; 
+    // UppercaseContract visibility lowercaseVariable
+
     function createSimpleStorageContract() public {
-        // How can this contract be made aware of the SimpleStorage contract? 
+        simpleStorage = new SimpleStorage();
     }
 }
