df177eb8d41a4dc6be640965bf1954eb00d482b1
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-11-08 08:51:09 -0700

    instance and import

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -2,32 +2,7 @@
 
 pragma solidity ^0.8.18;
 
-contract SimpleStorage {
-
-    uint256 public myFavoriteNumber;
-
-    struct Person{
-        uint256 favoriteNumber;
-        string name;
-    }
-
-    Person[] public listOfPeople;
-
-    mapping(string => uint256) public nameToFavoriteNumber;
-
-    function store(uint256 _favoriteNumber) public {
-        myFavoriteNumber = _favoriteNumber;
-    }
-
-    function retrieve() public view returns (uint256){
-        return myFavoriteNumber;
-    }
-
-    function addPerson(string memory _name, uint256 _favoriteNumber) public {
-        listOfPeople.push(Person(_favoriteNumber, _name));
-        nameToFavoriteNumber[_name] = _favoriteNumber;
-    }
-}
+import {SimpleStorage} from "./SimpleStorage.sol";
 
 contract StorageFactory{
 
