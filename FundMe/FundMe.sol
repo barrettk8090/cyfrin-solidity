54258221eec56fbed9794745aa8fefd2f54a53aa
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-11-10 11:10:58 -0700

    require keyword

--- FundMe/FundMe.sol
+++ FundMe/FundMe.sol
@@ -1,3 +1,19 @@
-// Get funds from users
-// Withdraw funds to the owner of the contract
-// Set a minimum funding value in USD
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+contract FundMe {
+
+    function fund() public payable {
+        // Allow users to send $
+        // Have a minimum $ sent
+        // How do we send ETH to this contract?
+        require (msg.value > 1e18, "didn't send enough ETH"); // 1e18 = 1ETH = 1000000000000000000 wei = 1 * 10 **18
+
+        // What is a revert? 
+        // Undo any actions that have been done, and send the remaining gas back. 
+    }
+
+    // function withdraw() public {}
+
+}
