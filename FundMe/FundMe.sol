fe7bf67e730cbc3146c9c2767ff898f9ba840d10
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-11-14 10:18:25 -0700

    interfaces intro

--- FundMe/FundMe.sol
+++ FundMe/FundMe.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.18;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
 
     function fund() public payable {
@@ -15,5 +17,10 @@ contract FundMe {
     }
 
     // function withdraw() public {}
+    function getPrice() public{
+        // Address - 0x694AA1769357215DE4FAC081bf1f309aDC325306 
+        // ABI - 
 
+    }
+    function getConversionRate ()public {}
 }
